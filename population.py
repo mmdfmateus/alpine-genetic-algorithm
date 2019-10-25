bec9ab69416b067ed74c2e4ccb1529d53aa18069
Aluno <example@email.com>
2019-10-25 08:54:54 -0300

    fix matrix to return float numbers

--- population.py
+++ population.py
@@ -1,11 +1,10 @@
 import random
 
 def createPopulation(size):
+    '''Returns a matrix of the following size. Each line is as follows [x1, x2, fitness, Σfitness]'''
     population = [[0 for x in range(4)] for y in range(size)]
 
     for i in range(size):
-        population[i][0] = random.randint(0, 10)
-        population[i][1] = random.randint(0, 10)
+        population[i][0] = round(random.uniform(0, 10), 3)
+        population[i][1] = round(random.uniform(0, 10), 3)
     return population
-
-print(createPopulation(10))
