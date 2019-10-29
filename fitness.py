7cf1eebdef25359299558409e9143b8d7df47ffa
Mateus Mourão <mmdfmateus@gmail.com>
2019-10-29 08:41:55 -0300

    add population fitness calculation

--- fitness.py
+++ fitness.py
@@ -5,6 +5,17 @@ def calculateFitness(xVec):
     prod = 1
     for x in xVec:
         prod *= math.sqrt(x)*math.sin(x)
+
+    prod += 10  # adding 10 so fitness will never be a negative value
     return prod
 
-calculateFitness([7.917, 7.917])
+def calculatePopulationFitness(population, size):
+    fitnessSum = 0
+    for i in range(size):
+        fitness = calculateFitness([population[i][0], population[i][1]])
+        fitnessSum += fitness
+        population[i][2] = fitness
+        population[i][3] = fitnessSum
+    return population
+
+# print(calculateFitness([7.917, 7.917]))
--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import population
+import fitness
 
 mutationRate = 0.1
 crossoverRate = 0.7
 
-print(population.createPopulation(10))
+size = 10
+
+print("\n### INITIAL POPULATION ###\n")
+currentPopulation = population.createPopulation(size)
+print(currentPopulation)
+print()
+print("\n### POPULATION FITNESS ###")
+currentPopulation = fitness.calculatePopulationFitness(currentPopulation, size)
+print()
+print(currentPopulation)
+# print(population.createPopulation(10))
